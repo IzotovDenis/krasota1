c6ba9f7dd5412d859b1c5ea7cbee6f52b2b36d42
IzotovDenis <izotov87@gmail.com>
2018-07-03 16:13:36 +1000

    add countersa

--- app/controllers/v1/search_controller.rb
+++ app/controllers/v1/search_controller.rb
@@ -7,7 +7,15 @@ class V1::SearchController <  V1Controller
         @ids = Item.search_for_ids(@query, set_options)
         @count = Item.search_for_ids(@query, set_options_ids)
         @order = "idx(array#{@ids.to_s}, items.id)::int" if @count.length > 0
-        @items = Item.where(:id=>@ids).order("#{@order}")
+        @items = Item.where(:id=>@ids).order("#{@order}").
+        joins("LEFT OUTER JOIN likes ON (likes.item_id = items.id AND likes.user_id = #{current_user ? current_user.id : 0})").
+        select("
+        items.title,
+        items.price,
+        items.image,
+        items.uid, 
+        CASE coalesce(likes.id, 0) WHEN 0 THEN 'false'::boolean ELSE 'true' END AS user_like, 
+        items.id")
         render json: {success: true, items:@items, total_entries:@count.length, query_string:@query_text, pageLoaded: params[:page]}, serializer: nil, adapter: false
         rescue => e
           render json: {error: true}, status: 500

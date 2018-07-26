fcfb9fa6ffe7e598b3e83fae0f6041e84157f89f
IzotovDenis <izotov87@gmail.com>
2018-07-26 14:48:03 +1000

    add sms

--- app/commands/sms_commands.rb
+++ app/commands/sms_commands.rb
@@ -6,7 +6,7 @@ class SMSCommands
         request = Net::HTTP.new(uri.host, uri.port)
         request.use_ssl = true
         request.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        response = request.get("/sms/send?api_id=#{Rails.application.secrets.sms_secret_key}&to=#{tel}&msg=Ваш+код:+#{pin}&json=1&test=1")
+        response = request.get("/sms/send?api_id=#{Rails.application.secrets.sms_secret_key}&to=#{tel}&msg=Ваш+код:+#{pin}&json=1")
         response = JSON.parse(response.body)
         puts response["sms"][tel.to_s]["status"]
         if response["sms"][tel.to_s]["status"] == "OK"
